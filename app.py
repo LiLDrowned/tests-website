01f7e9acf71d71b9b56968171c9626574c767682
Jakub Hlavačka <jakubhlavacka123@gmail.com>
2023-02-28 14:04:32 +0100

    not very smart way to wait before db starts

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from website import create_app
 from flask import redirect, url_for
+import time
+
+# not a clever way to wait for db
+# in future maybe replace with this https://stackoverflow.com/questions/35069027/docker-wait-for-postgresql-to-be-running
+time.sleep(20) 
 
 app = create_app()
 
