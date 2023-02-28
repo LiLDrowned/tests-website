074cfe29ec76544b003a8d1c77d37e7918dbbe89
Jakub Hlavačka <jakubhlavacka123@gmail.com>
2023-02-28 13:32:08 +0100

    update settings.py

--- website/settings.py
+++ website/settings.py
@@ -1,5 +1,5 @@
 import os
 
-DB_PASSWORD = str(os.getenv('DB_PASSWORD') or 'rootpassword')
-DB_USERNAME = str(os.getenv('DB_USERNAME') or 'postgres')
+DB_PASSWORD = str(os.getenv('POSTGRES_PASSWORD') or 'rootpassword')
+DB_USERNAME = str(os.getenv('POSTGRES_USER') or 'postgres')
 SECRET_KEY = str(os.getenv('SECRET_KEY') or 'chlebik s maslom')
